787ea22eb0701c8ae4b08963641f6eadb82e8a87
aditi <aditisharma29062004@gmail.com>
2025-11-18 19:20:14 +0530

    dataypes

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+let name="aditi"
+let age=21
+let isLoggedIn=true
+let rollno;
+let city=null;
+
+
+//number=>2 to power 53 
+//bigInt=>bigger than number
+//string=>""
+//boolean =>true/false
+//null =>stand alone value
+//undefined =>no value assigned
+//symbol =>uniqueness(react)
+//object
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof rollno)
+console.log(typeof city)
+
+/*
+you can even write values there or even type only
+*/
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof "vaibhav")
+console.log(typeof 123)
+console.log(typeof true)
