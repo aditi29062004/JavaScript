6c288fb5411131bb3860ae64332ba3af7a711792
aditi <aditisharma29062004@gmail.com>
2025-11-18 19:04:38 +0530

    "new file added"

---
+++ test.js
@@ -0,0 +1,24 @@
+let accountId=12231
+let accountEmail="aditisharma266@gmail.com"
+let accountPass="aditi"
+const city="Delhi"
+
+/*
+we dont use var keyword
+because it creates problem with block and functional scope
+Also we can create
+city="delhi" as well 
+but that is not recommended.
+*/
+
+//city="bangalore"
+accountId=56547
+
+/*
+const-can't be changed
+let can be changed
+*/
+
+let marks;
+
+console.log([accountId,accountEmail,accountPass,city,marks]);
