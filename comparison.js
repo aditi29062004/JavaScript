a4b4d72a182162ffde4332f629065a4d694fc4f8
aditi <aditisharma29062004@gmail.com>
2025-11-22 12:52:02 +0530

    operators

---
+++ comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//undefined will give false with every comparisons
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+/*
+=== :equal with type same
+==: equal value 
+*/
+console.log("2"===2)
+console.log("2"==2)
---
+++ conversionSpecial.js
@@ -0,0 +1,16 @@
+//---------operators-------------//
+
+console.log(3+2);
+console.log(3-2);
+console.log(3*2);
+console.log(3**2);
+console.log(3/2);
+console.log(3%2);
+
+//Special Case
+console.log(1+2)
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
